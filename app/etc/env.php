28dcda063e0bbc45a6b771d14f6fb73c72c159b6
Veeraswami Kandregula <happybooksproject@gmail.com>
2017-10-15 22:03:40 +1100

    basics

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_kjgv0u',
+  ),
+  'crypt' => 
+  array (
+    'key' => '6657d0cc0cc38ec78ed65413c4f8e478',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'basic',
+        'username' => 'root',
+        'password' => 'root',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 15 Oct 2017 10:45:26 +0000',
+  ),
+);
