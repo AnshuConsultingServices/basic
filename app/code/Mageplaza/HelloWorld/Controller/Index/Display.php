b139d47e0edfc1a0842502dfc8546db455090551
Veeraswami Kandregula <happybooksproject@gmail.com>
2017-10-26 20:49:36 +1100

    images got uploaded to server

--- app/code/Mageplaza/HelloWorld/Controller/Index/Display.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Display.php
@@ -1,54 +1,96 @@
 <?php
 namespace Mageplaza\HelloWorld\Controller\Index;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
+
 class Display extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
+    protected $_filesystem;
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory)
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Framework\Filesystem $fileSystem)
     {
         $this->_pageFactory = $pageFactory;
+        $this->_filesystem = $fileSystem;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // instance of object manager
-$product = $objectManager->create('\Magento\Catalog\Model\Product');
-$product->setSku('my-skuw'); // Set your sku here
-$product->setName('Sample Simple Producst'); // Name of Product
-$product->setAttributeSetId(9); // Attribute set id
-$product->setStatus(1); // Status on product enabled/ disabled 1/0
-$product->setWeight(10); // weight of product
-$product->setVisibility(4); // visibilty of product (catalog / search / catalog, search / Not visible individually)
-$product->setTaxClassId(0); // Tax class id
-$product->setTypeId('simple'); // type of product (simple/virtual/downloadable/configurable)
-$product->setPrice(100); // price of product
-//$product->setAttribute('author_name','test');
-//$product->setAttribute('test','author_name');
+                    $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // instance of object manager
 
-$attrCode = 'author_name';
-$valueText = ('PATAGONIA');
-//$valueId = $product->getResource()->getAttribute($attrCode)->getSource()->getOptionId($valueText);
-//$product->setData($attrCode, $valueText);
-$product->setData('author_name','foobar');
+                $post = $this->getRequest()->getPost();
+
+        if ($post) {
+            // Retrieve your form data
+            $bookname   = $post['bookname'];
+            $author    = $post['author'];
+            \Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( '1');
+            //if ($_FILES['']) {
+
+if (isset($_FILES['front-cover'])) {
+
+\Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( '2');
+            /** @var \Magento\Catalog\Model\Product\Media\Config $config */
+            $config = $objectManager->get('Magento\Catalog\Model\Product\Media\Config');
+            /** @var \Magento\Framework\Filesystem\Directory\WriteInterface $mediaDirectory */
+            $mediaDirectory = $objectManager->get('Magento\Framework\Filesystem')
+                ->getDirectoryWrite(DirectoryList::MEDIA);
+\Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( '3');
+            $baseTmpMediaPath = $config->getBaseTmpMediaPath();
+            //$mediaDirectory->create($baseTmpMediaPath);
+            //copy(__DIR__ . '/product_image.png', $mediaDirectory->getAbsolutePath($baseTmpMediaPath . '/product_image.png'));
+
+            \Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( $baseTmpMediaPath);
+//\Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( $mediaDirectory);
+        
+ // init uploader model.
+                $uploader = $this->_objectManager->create(
+                    'Magento\MediaStorage\Model\File\Uploader',
+                    ['fileId' => 'front-cover']
+                );
+                $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+                $uploader->setAllowRenameFiles(true);
+                $uploader->setFilesDispersion(true);
+
+                $uploader->save($mediaDirectory->getAbsolutePath($baseTmpMediaPath . '/product_image.png'));
+
+}
+            
 
 
-//$product->getResource()->saveAttribute($product,'author_name');
-$product->setStockData(
-                        array(
-                            'use_config_manage_stock' => 0,
-                            'manage_stock' => 1,
-                            'is_in_stock' => 1,
-                            'qty' => 999999999
-                        )
-                    );
+
+
+            $product = $objectManager->create('\Magento\Catalog\Model\Product');
+
+            $product->setSku($bookname); // Set your sku here
+            $product->setName($bookname); // Name of Product
+            $product->setAttributeSetId(9); // Attribute set id
+            $product->setStatus(1); // Status on product enabled/ disabled 1/0
+            //$product->setWeight(10); // weight of product
+            $product->setVisibility(4); // visibilty of product (catalog / search / catalog, search / Not visible individually)
+            $product->setTaxClassId(0); // Tax class id
+            $product->setTypeId('simple'); // type of product (simple/virtual/downloadable/configurable)
+            $product->setPrice(100); // price of product
+
+            $product->setData('author_name',$author);
+
+
+            //$product->getResource()->saveAttribute($product,'author_name');
+            $product->setStockData(
+                                    array(
+                                        'use_config_manage_stock' => 0,
+                                        'manage_stock' => 1,
+                                        'is_in_stock' => 1
+                                    )
+                                );
 //$this->_log("am here");
-//\Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->debug( $product->getAttributes())
-;
+
 //echo $product->getCustomAttributesCodes();
 $product->save();
+}
         return $this->_pageFactory->create();
 return;
     }
